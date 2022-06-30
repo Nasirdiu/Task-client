dc83f178c69e5fcb6a7213f80ef7f2f9825f70be
nasirdiu <nasir.joypurhat@gmail.com>
2022-06-30 21:56:12 +0600

    aa

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/user`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure you want to delete");
+    if (proceed) {
+      console.log("deleting user id", id);
+      const url = `http://localhost:5000/user/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            const remaining = users.filter((user) => user._id !== id);
+            setUsers(remaining);
+          }
+        });
+    }
+  };
+  return (
+    <div>
+      <h1>All user data:{users.length}</h1>
+
+     {
+        users.map(user=><div>
+             <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{user.name}</td>
+              <td><button className="btn btn-sm btn-primary">Update</button></td>
+              <td><button className="btn btn-sm btn-primary" onClick={() => handleDelete(user._id)}>Delete</button></td>
+            </tr>
+
+            
+
+            
+          </tbody>
+        </table>
+      </div>
+        </div>)
+     }
+    </div>
+  );
 };
 
 export default Home;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const ToDo = () => {
-    return (
-        <div>
-            <h1>Todo</h1>
-        </div>
-    );
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const name=event.target.name.value;
+        const user={name};
+        fetch('http://localhost:5000/user', {
+            method: 'POST', // or 'PUT'
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+          })
+          .then(response => response.json())
+          .then(data => {
+            alert('add user successfully');
+            event.target.reset();
+          })
+    }
+  return (
+    <div className="text-center mt-40">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="TODO"
+          name="name"
+          class="input input-bordered w-full max-w-xs"
+        /> <br />
+        <button className="btn btn-primary mt-5">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default ToDo;
