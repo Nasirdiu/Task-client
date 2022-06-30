dd0a0f4ad2436b55d40e870de6a860308ef6ae5a
nasirdiu <nasir.joypurhat@gmail.com>
2022-06-30 16:17:57 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+    const menu = (
+        <>
+          <li>
+            <Link to="/task">Completed Tasks</Link>
+          </li>
+          <li>
+            <Link to="todo" spy={true} smooth={true}>
+            To-Do
+            </Link>
+          </li>
+          <li>
+            <Link to="calendar" spy={true} smooth={true}>
+            Calendar
+            </Link>
+          </li>
+         
+          
+        </>
+      );
+  return (
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+           {menu}
+          </ul>
+        </div>
+        
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">
+          {menu}
+        </ul>
+      </div>
+    
+    </div>
+  );
+};
+
+export default Navbar;
